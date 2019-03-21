ae3e14005bdc341fa180192f871ee86de3b06651
Vinh Hoang <vinhhoang51@gmail.com>
2019-03-21 11:19:10 +0700

    index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    morgan = require('morgan');
+var cors = require('cors');
+app = express();
+app.use(express.static('Public'));
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cors());
+var port = process.env.PORT || 8088;
+var http = require('http').Server(app);
+http.listen(port, () => {
+    console.log('Connected at port:' + port);
+})
