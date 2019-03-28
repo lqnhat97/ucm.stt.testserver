c4835ac6854b91c27d3be30c1958d5f31586883b
nhatlq <lqnhat97@gmail.com>
2019-03-28 14:48:31 +0700

    fix api

--- Controller/PatientController.js
+++ Controller/PatientController.js
@@ -38,7 +38,7 @@ router.get('/thongtinkhambenh/:id', async (req, res) => {
                 });
             }
         } else {
-            thongTinKhamBenh = ['null'];
+            //thongTinKhamBenh = ['null'];
         }
     })
 
@@ -49,8 +49,8 @@ router.get('/thongtinkhambenh/:id', async (req, res) => {
             for (i = 0; i < Object.keys(rows.recordsets[0]).length; i++) {
                 stt = rows.recordsets[0][i].STTPhongCLS;
                 maPhongCls = rows.recordsets[0][i].SoPhong;
-                thoiGianDuKien = rows.recordsets[0][0].Gio
-                tenPhong = rows.recordsets[0][0].TenLoai;
+                thoiGianDuKien = rows.recordsets[0][i].Gio
+                tenPhong = rows.recordsets[0][i].TenLoai;
                 await db.loadPhongClsHienTai(rows.recordsets[0][i].IDPhong).then(rows => {
                     console.log(rows);
                     //kiem tra co du lieu khong
@@ -73,10 +73,18 @@ router.get('/thongtinkhambenh/:id', async (req, res) => {
     thongTinKhamBenh.push(lamSang);
     thongTinKhamBenh.push(canLamSang);
 
-    res.json({
-        lamSang: thongTinKhamBenh[0],
-        canLamSang: thongTinKhamBenh[1]
-    });
+    if (thongTinKhamBenh[0] == 'null') {
+        res.json({
+            thongTinKhamBenh: 'null'
+        });
+        res.end(200);
+    } else {
+        res.json({
+            lamSang: thongTinKhamBenh[0],
+            canLamSang: thongTinKhamBenh[1]
+        });
+        res.end(200);
+    }
 })
 
 module.exports = router;
