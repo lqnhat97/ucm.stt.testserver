bbe58b5f007df6c9b1fef9e66083ee35e65df5ca
Vũ Thành Nam <yvisvu@gmail.com>
2019-05-20 22:50:59 +0700

    script DB\full_new.sql script DB\selectBang.sql

--- script DB/selectBang.sql
+++ script DB/selectBang.sql
@@ -62,17 +62,37 @@ END;
 GO
 
 EXEC XuatDanhSachPhongKhamTheoChuyenKhoa "PH"
+
+exec TinhTrangHienTaiTheoChuyenKhoa'ph'
 ----------------------------------
+select * from SoThuTuPhongKham as STT left join SoHienThiPhongKham as SHT on STT.BanKham=SHT.BanKham and SHT.PhongKham= STT.PhongKham
+
 CREATE PROC [dbo].[TinhTrangHienTaiTheoChuyenKhoa]
 @IDchuyenKhoa nvarchar(10)
 AS
 BEGIN
-declare @temp nvarchar(10)
-set @temp=(select distinct sht.PhongKham from SoHienThiPhongKham sht inner join PhongKhamChuyenKhoa pkck on pkck.ID= sht.PhongKham
-where pkck.ChuyenKhoa=@IDchuyenKhoa)
-if(@temp is not null)
+declare @temp int,@temp2 int
+set @temp=(select distinct count(*) from SoHienThiPhongKham sht inner join PhongKhamChuyenKhoa pkck on pkck.ID= sht.PhongKham
+			where pkck.ChuyenKhoa=@IDchuyenKhoa);
+set @temp2 =(select count(*)
+			from (select distinct pkck.ChuyenKhoa,lk.Phong ,LK.IDBan from SoThuTuPhongKham as STT join LichKhamBacSi as LK 
+							 on STT.BanKham=LK.IDBan and LK.Phong= STT.PhongKham 
+							 inner join PhongKhamChuyenKhoa pkck on pkck.ID= STT.PhongKham
+							 where pkck.ChuyenKhoa=@IDchuyenKhoa) as A);
+if(@temp=0)
 begin
-select  B.PhongKham, bk.SoBan, B.BanKham,  A.stt AS STTHienTai, B.GioMax, bs.ID as IDBacSi, bs.HoVaTen as BacSi,BN.ID as IDBenhNhan, (BN.Ho + ' ' +BN.TenLot + ' ' + BN.Ten) AS BenhNhan
+select  distinct pkck.ChuyenKhoa,lk.Phong as PhongKham ,bk.SoBan,bs.ID as IDBacSi,  bs.HovaTen as BacSi,0 as SoHienTai, N'Chưa có bệnh nhân' AS BenhNhan
+	from LichKhamBacSi lk 
+	inner join SoThuTuPhongKham as STT on STT.BanKham=lk.IDBan and STT.PhongKham=lk.Phong
+	inner join BanKham bk on bk.IDBan=lk.IDBan
+	inner join BacSi bs on bs.ID=lk.IDBacSi
+	inner join PhongKhamChuyenKhoa pkck on pkck.ID= bk.Phong
+	where pkck.ChuyenKhoa=@IDchuyenKhoa
+	order by  pkck.ChuyenKhoa,lk.Phong ,bk.SoBan;
+end;
+else if(@temp2=0)
+begin
+select  pkck.ChuyenKhoa,B.PhongKham,bk.SoBan, bs.ID as IDBacSi, bs.HoVaTen as BacSi,  A.stt AS SoHienTai, (BN.Ho + ' ' +BN.TenLot + ' ' + BN.Ten) AS BenhNhan, B.GioMax
 from ( select sht.PhongKham, sht.BanKham, max(sht.Gio) as GioMax
 		from SoHienThiPhongKham sht
 		group by sht.PhongKham, sht.BanKham ) as B
@@ -83,18 +103,52 @@ inner join BanKham bk on bk.IDBan=B.BanKham and bk.Phong=B.PhongKham
 inner join LichKhamBacSi lk on lk.IDBan=B.BanKham and lk.Phong=B.PhongKham
 inner join BacSi bs on bs.ID=lk.IDBacSi
 inner join PhongKhamChuyenKhoa pkck on pkck.ID= bk.Phong
-where pkck.ChuyenKhoa=@IDchuyenKhoa
+where pkck.ChuyenKhoa=@IDchuyenKhoa;
 end;
-else if(@temp is null)
+else if(((@temp2 >@temp ) and @temp!=0) or ((@temp2<@temp ) and @temp2!=0))
 begin
-select    pkck.ChuyenKhoa,lk.Phong ,bk.SoBan,  bs.HoVaTen as BacSi, 0 as SoHienTai, N'Không có bệnh nhân' AS BenhNhan
-from LichKhamBacSi lk 
-inner join BanKham bk on bk.IDBan=lk.IDBan
+select  pkck.ChuyenKhoa,B.PhongKham,bk.SoBan, bs.ID as IDBacSi, bs.HoVaTen as BacSi,  A.stt AS SoHienTai, (BN.Ho + ' ' +BN.TenLot + ' ' + BN.Ten) AS BenhNhan, B.GioMax
+from ( select sht.PhongKham, sht.BanKham, max(sht.Gio) as GioMax
+		from SoHienThiPhongKham sht
+		group by sht.PhongKham, sht.BanKham ) as B
+inner join SoHienThiPhongKham as A on A.BanKham=B.BanKham and A.PhongKham=b.PhongKham and A.Gio=B.GioMax
+inner join PhieuKham pk on pk.IDPhieuKham= A.IDPhieuKham
+inner join BenhNhan BN on IDBenhNhan=BN.ID
+inner join BanKham bk on bk.IDBan=B.BanKham and bk.Phong=B.PhongKham
+inner join LichKhamBacSi lk on lk.IDBan=B.BanKham and lk.Phong=B.PhongKham
 inner join BacSi bs on bs.ID=lk.IDBacSi
 inner join PhongKhamChuyenKhoa pkck on pkck.ID= bk.Phong
-where pkck.ChuyenKhoa=@IDChuyenKhoa;
+where pkck.ChuyenKhoa=@IDchuyenKhoa;
+select  distinct pkck.ChuyenKhoa,lk.Phong as PhongKham ,bk.SoBan,bs.ID as IDBacSi,  bs.HovaTen as BacSi,0 as SoHienTai, N'Chưa có bệnh nhân' AS BenhNhan
+	from LichKhamBacSi lk 
+	inner join SoThuTuPhongKham as STT on STT.BanKham=lk.IDBan and STT.PhongKham=lk.Phong
+	inner join BanKham bk on bk.IDBan=lk.IDBan
+	inner join BacSi bs on bs.ID=lk.IDBacSi
+	inner join PhongKhamChuyenKhoa pkck on pkck.ID= bk.Phong
+	where pkck.ChuyenKhoa=@IDchuyenKhoa and bs.ID not in (select lich.IDBacSi from SoHienThiPhongKham as HT join BanKham as ban 
+																		on HT.BanKham=ban.IDBan and HT.PhongKham=ban.Phong
+																		join PhongKhamChuyenKhoa as phong on phong.ID=ban.Phong
+																		join LichKhamBacSi as lich on lich.IDBan=HT.BanKham and lich.Phong=HT.PhongKham
+																		join BacSi as bs on bs.ID=lich.IDBacSi
+																	 where phong.ChuyenKhoa =@IDchuyenKhoa)
+	order by  pkck.ChuyenKhoa,lk.Phong ,bk.SoBan;
 end;
-END;
+else if(@temp =@temp2)
+begin
+select  pkck.ChuyenKhoa,B.PhongKham,bk.SoBan, bs.ID as IDBacSi, bs.HoVaTen as BacSi,  A.stt AS SoHienTai, (BN.Ho + ' ' +BN.TenLot + ' ' + BN.Ten) AS BenhNhan, B.GioMax
+from ( select sht.PhongKham, sht.BanKham, max(sht.Gio) as GioMax
+		from SoHienThiPhongKham sht
+		group by sht.PhongKham, sht.BanKham ) as B
+inner join SoHienThiPhongKham as A on A.BanKham=B.BanKham and A.PhongKham=b.PhongKham and A.Gio=B.GioMax
+inner join PhieuKham pk on pk.IDPhieuKham= A.IDPhieuKham
+inner join BenhNhan BN on IDBenhNhan=BN.ID
+inner join BanKham bk on bk.IDBan=B.BanKham and bk.Phong=B.PhongKham
+inner join LichKhamBacSi lk on lk.IDBan=B.BanKham and lk.Phong=B.PhongKham
+inner join BacSi bs on bs.ID=lk.IDBacSi
+inner join PhongKhamChuyenKhoa pkck on pkck.ID= bk.Phong
+where pkck.ChuyenKhoa=@IDchuyenKhoa;
+end
+end
 GO
 
 /****** Object:  StoredProcedure [dbo].[TinhTrangHienTaiTheoPhongKhamThuocChuyenKhoa]    Script Date: 5/16/2019 1:09:10 AM ******/
